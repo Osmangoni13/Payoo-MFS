ee7a8ce55ee7292d0b99529b421f7c39996322f3
Osmangoni13 <osmanmonju@gmail.com>
2024-09-20 12:06:14 +0600

    added money in the main blance

--- JS/home.js
+++ JS/home.js
@@ -8,5 +8,25 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     //get pin number
     const pinNumberInput = document.getElementById('input-pin-number').value;
 
+    //verify pin number
+    //wrong way to valdate pin number,it's uses for learning purpose/
+    if (pinNumberInput === '1234') {
+        console.log('adding money to your account')
+        //get the current blance
+        const blance = document.getElementById("account-blance").innerText;
+        console.log(blance)
+
+        //step-5: addmoneyInput with blance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const blanceNumber = parseFloat(blance);
+        const newBlance = addMoneyNumber + blanceNumber;
+        console.log(typeof newBlance)
+
+        // step-6: Update the blance in the UI/DOM
+        document.getElementById('account-blance').innerText = newBlance;
+    } else {
+        alert('Faild to add Money! Please try again!')
+    }
+
 
 })
