efff2b243f01d3816e79b0eeb4a15b0de614dd3c
Osmangoni13 <osmanmonju@gmail.com>
2024-09-20 11:24:09 +0600

    payoo MFS

---
+++ JS/home.js
@@ -0,0 +1,7 @@
+//add money to the account
+
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    //prevent page relode after login
+    event.preventDefault();
+    console.log('clicked')
+})
---
+++ JS/login.js
@@ -0,0 +1,15 @@
+document.getElementById('button-login').addEventListener('click', function (event) {
+
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('You are loged in');
+        window.location.href = './home.html'
+    }
+    else {
+        alert('Wrong phone number of password');
+    }
+})
